7455cac602b196951c598b55fe362058e290debc
Ashiq <web3.0.ashiq@gmail.com>
2023-11-07 01:45:08 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,16 @@
-import express, { Application,Request ,Response} from 'express';
-import cors from 'cors';
-const app:Application = express();
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-
-app.use(cors());
+app.use(cors())
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //testing
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: any) => {
   res.send('working succesfully!')
 })
 
-
-
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 import config from './config/index'
-import app from "./app";
-
-
-
+import app from './app'
 
 async function boostrap() {
   try {
-     await mongoose.connect(config.database_url as string);
-     console.log(` Database connectded seccesfully`);
+    await mongoose.connect(config.database_url as string)
+    console.log(` Database connectded seccesfully`)
 
-     app.listen(config.port, () => {
-  console.log(`Example app listening on port ${config.port}`)
-})
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log("Failed to connect database", err);
+    console.log('Failed to connect database', err)
   }
-
- 
 }
 
 boostrap()
