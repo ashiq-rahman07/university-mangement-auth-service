f3efff43187a2cf7a39fa4797c6084b16b3a9643
Ashiq <web3.0.ashiq@gmail.com>
2023-11-07 00:51:37 +0600

    (DONE) setup express, mongoose, cors, parser

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application,Request ,Response} from 'express';
+import cors from 'cors';
+const app:Application = express();
+
+
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//testing
+app.get('/', (req:Request, res:Response) => {
+  res.send('working succesfully!')
+})
+
+
+
+export default app;
