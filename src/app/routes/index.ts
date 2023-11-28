028eb3d14b15d4153c531463b289cd29171917cf
Ashiqur Rahman <108370520+ashiq-rahman07@users.noreply.github.com>
2023-11-27 17:05:31 -0800

    User as admin module created

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,6 +5,8 @@ import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFacult
 import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 import { FacultyRoutes } from '../modules/faculty/faculty.route';
 import { StudentRoutes } from '../modules/student/student.route';
+import { AdminRoutes } from '../modules/admin/admin.route';
+import { ManagementDepartmentRoutes } from '../modules/managementDepartment/managementDepartment.route';
 
 const router = express.Router();
 
@@ -26,12 +28,20 @@ const moduleRoutes = [
     route: academicDepartmentRoutes,
   },
   {
+    path: '/management-departments',
+    route: ManagementDepartmentRoutes,
+  },
+  {
     path: '/faculties',
     route: FacultyRoutes,
   },
   {
     path: '/students',
     route: StudentRoutes,
+  },
+  {
+    path: '/admins',
+    route: AdminRoutes,
   },
 ];
 moduleRoutes.forEach(route => router.use(route.path, route.route));
--- src/config/index.ts
+++ src/config/index.ts
@@ -9,4 +9,5 @@ export default {
   database_url: process.env.DATABASE_URL,
   default_student_pass: process.env.DEAFULT_STUDENT_PASS,
   default_faculty_pass: process.env.DEAFULT_FACULTY_PASS,
+  default_admin_pass: process.env.DEAFULT_ADMIN_PASS,
 };
